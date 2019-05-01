032cfff88a6994c8070fb6351a0909d9e02fd8b3
Nissan Holzer <nissan.holzer@gmail.com>
2019-05-01 20:08:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,10 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get '/multiply/:num1/:num2' do
-    @num1 = params[:num1].to_i 
-    @num2 = params[:num2].to_i
-    @product = num1 * num2
-    "#{@product}"
+  get "/multiply/:num_1/:num_2" do
+    "#{params[:num_1].to_i * params[:num_2].to_i}"
   end
 end
